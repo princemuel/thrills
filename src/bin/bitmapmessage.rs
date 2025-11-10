c19444eef68c33e313fdad8919249357979490ca
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-11-10 13:41:39 +0100

    feat: implement Bitmap Message challenge

---
+++ src/bin/bitmapmessage.rs
@@ -0,0 +1,69 @@
+//! Bitmap Message, by Prince Muel info@princemuel.dev
+//! Displays a text message according to the provided bitmap image.
+//! This code is available at https://github.com/princemuel/thrills
+//! Tags: tiny, beginner, artistic
+use std::io::{self, Write as _};
+
+use unicode_segmentation::UnicodeSegmentation;
+
+const BITMAP: &str = r#"
+....................................................................
+   **************   *  *** **  *      ******************************
+  ********************* ** ** *  * ****************************** *
+ **      *****************       ******************************
+          *************          **  * **** ** ************** *
+           *********            *******   **************** * *
+            ********           ***************************  *
+   *        * **** ***         *************** ******  ** *
+               ****  *         ***************   *** ***  *
+                 ******         *************    **   **  *
+                 ********        *************    *  ** ***
+                   ********         ********          * *** ****
+                   *********         ******  *        **** ** * **
+                   *********         ****** * *           *** *   *
+                     ******          ***** **             *****   *
+                     *****            **** *            ********
+                    *****             ****              *********
+                    ****              **                 *******   *
+                    ***                                       *    *
+                    **     *                    *
+...................................................................."#;
+
+fn main() -> io::Result<()> {
+    println!("Bitmap Message, by Al Sweigart al@inventwithpython.com");
+    println!("Enter the message to display with the bitmap.");
+
+    let message = read_line()?;
+    let message = message.trim();
+
+    let graphemes: Vec<&str> = UnicodeSegmentation::graphemes(message, true).collect();
+    let n = graphemes.len();
+
+    for line in BITMAP.lines() {
+        for (i, bit) in line.char_indices() {
+            if bit == ' ' {
+                print!(" ");
+            } else {
+                print!("{}", graphemes[i % n]);
+            }
+        }
+
+        println!();
+    }
+
+    Ok(())
+}
+
+fn read_line() -> io::Result<String> {
+    loop {
+        print!("> ");
+        io::stdout().flush()?;
+
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer)?;
+
+        if !buffer.trim().is_empty() {
+            return Ok(buffer);
+        }
+    }
+}
